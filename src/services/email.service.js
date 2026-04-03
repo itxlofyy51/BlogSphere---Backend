334664b6f00ffb548c8eabcbff8048b93376496a
itxlofyy51 <sahernawaz09@gmail.com>
2026-04-03 18:01:21 +0500

    Update email.service.js

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,18 +2,22 @@ const nodemailer = require("nodemailer");
 const config = require("../config/config");
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 587,
   secure: false, 
   auth: {
     user: config.EMAIL_USER,
     pass: config.EMAIL_PASS,
   },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+  dnsLookup: (hostname, options, callback) => {
+    require('dns').lookup(hostname, { family: 4 }, callback); // Forces IPv4
+  },
   tls: {
-    rejectUnauthorized: false // This helps bypass cloud network restrictions
-  }
-});
+    rejectUnauthorized: false 
+  }});
 transporter.verify((error,success)=>{ 
     if(error){
         console.log("Error setting up email transporter",error);
